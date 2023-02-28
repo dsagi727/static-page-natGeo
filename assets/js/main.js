264b147c98c42d7f21b65434f896648ce11c8f41
dsagi727 <dsagi727@gmail.com>
2023-02-28 17:23:52 +0100

    fix navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict'
 
 //copy right actual year
 const paragraph = `
@@ -6,31 +6,15 @@ const paragraph = `
   Copyright
   National ${new Date().getFullYear()} Geographic Society. All rights reserved
 </p>
-`;
-document.getElementById("copyright").innerHTML = paragraph;
+`
+document.getElementById('copyright').innerHTML = paragraph
 
 //NAVBAR
 function navMenu() {
-  let nav = document.getElementById("myLinks");
-  let bars = document.getElementsByClassName("fa-bars");
-  let close = document.getElementsByClassName("fa-x");
-  if (nav.classList.contains("hideNav")) {
-    nav.style.display = "block";
-    nav.classList.add("showNav");
-    nav.classList.remove("hideNav");
-    close[0].classList.add("open");
-    setTimeout(() => {
-      bars[0].style.display = "none";
-      close[0].style.display = "block";
-    }, "1000");
+  let collapse = document.getElementById('myLinks')
+  if (collapse.style.display === 'block') {
+    collapse.style.display = 'none'
   } else {
-    nav.classList.add("hideNav");
-    nav.classList.remove("showNav");
-    bars[0].classList.add("open");
-    setTimeout(() => {
-      nav.style.display = "none";
-      close[0].style.display = "none";
-      bars[0].style.display = "block";
-    }, "1000");
+    collapse.style.display = 'block'
   }
 }
